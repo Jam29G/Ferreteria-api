984dcf786115719e7d4d11b60355cbdf49ec5423
Jamil Gutierrez <jamilgutierrez00@protonmail.com>
2022-10-09 21:39:22 -0600

    first commit

---
+++ src/main/java/com/dev/ferreteriaapi/FerreteriaapiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ferreteriaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/ferreteriaapi/FerreteriaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.ferreteriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
