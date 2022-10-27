4c83320431de66d5b7f00faefc43f2df4726dd4b
Edgardo Figueroa <edgardo_figue@hotmail.com>
2022-10-27 00:17:04 -0600

    Se creo EndPoint para consulta de Compras por Fecha de Inicio y Fin

--- src/main/java/com/dev/ferreteriaapi/controllers/CompraController.java
+++ src/main/java/com/dev/ferreteriaapi/controllers/CompraController.java
@@ -3,13 +3,17 @@ package com.dev.ferreteriaapi.controllers;
 import com.dev.ferreteriaapi.entities.Compra;
 import com.dev.ferreteriaapi.entities.DetalleProducto;
 import com.dev.ferreteriaapi.entities.Empresa;
+import com.dev.ferreteriaapi.entities.Producto;
 import com.dev.ferreteriaapi.services.interfaces.ICompraService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -29,6 +33,21 @@ public class CompraController {
     @PostMapping("")
     public ResponseEntity<Compra> create(@RequestBody() Compra compra) {
         return new ResponseEntity<>(compraService.create(compra), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/rangeDate")
+    public ResponseEntity<List<Compra>> getComprasByDates(@RequestParam("fechaInicio") String fechaInicio, @RequestParam("fechaFin") String fechaFin) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+        LocalDateTime start = LocalDateTime.parse(fechaInicio, formatter);
+        LocalDateTime end = LocalDateTime.parse(fechaFin, formatter);
+
+        if(!start.isBefore(end)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Rango de fechas invalido");
+        }
+
+        return new ResponseEntity<>(this.compraService.findCompraByDates(start, end), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/dev/ferreteriaapi/repository/CompraRepo.java
+++ src/main/java/com/dev/ferreteriaapi/repository/CompraRepo.java
@@ -1,10 +1,17 @@
 package com.dev.ferreteriaapi.repository;
 
 import com.dev.ferreteriaapi.entities.Compra;
+import com.dev.ferreteriaapi.entities.MovimCaja;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CompraRepo extends JpaRepository<Compra, Long> {
+
+    @Query("select c from Compra c where c.fechaCompra between :fechaInicio and :fechaFin")
+    List<Compra> findCompraByDates(@Param("fechaInicio") LocalDateTime fechaInicio, @Param("fechaFin") LocalDateTime fechaFin);
 
 }
